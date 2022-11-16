4d9b83c65acf6af0f974049f176785eb1ff10cbd
RoadKing159753 <111482881+RoadKing159753@users.noreply.github.com>
2022-11-16 20:24:38 +0530

    Update ec2single.tf

--- ec2single.tf
+++ ec2single.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
  #text
  
-resource "aws_s3_bucket" "unrestricted" {
+resource "aws_s3_bucket" "unrestricted1" {
   bucket = "examplebuckettftest"
   acl    = "private"
   versioning {
